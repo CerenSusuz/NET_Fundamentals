eec8825656e71a8cdbc02f1d32de689fe56fbd75
Ceren Susuz <ceren_susuz@epam.com>
2023-10-02 18:41:52 +0300

    added concatenation logic to console

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,9 @@
-﻿string username = Console.ReadLine();
+﻿using HelloWorldLibrary;
+
+string username = Console.ReadLine();
 Console.WriteLine($"Hello, {username}");
+Console.WriteLine("---------------------");
+ConcatLogic concatenationLogic = new();
+Console.WriteLine(concatenationLogic.Concat(username));
+Console.WriteLine("---------------------");
+Console.WriteLine(concatenationLogic.ConcatTypeTwo(username));
