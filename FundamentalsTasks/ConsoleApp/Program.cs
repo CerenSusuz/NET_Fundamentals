e763fa278ced9277ed8c7179bf45b406a6d0e0be
Ceren Susuz <ceren_susuz@epam.com>
2023-10-12 01:19:33 +0300

    added projects under fundamentals folder

---
+++ FundamentalsTasks/ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using StandardLibraryConcat;
+
+string username = Console.ReadLine();
+Console.WriteLine($"Hello, {username}");
+Console.WriteLine("---------------------");
+ConcatLogic concatenationLogic = new();
+Console.WriteLine(concatenationLogic.Concat(username));
+Console.WriteLine("---------------------");
+Console.WriteLine(concatenationLogic.ConcatTypeTwo(username));
---
+++ FundamentalsTasks/HelloWorldLibrary/ConcatLogic.cs
@@ -0,0 +1,26 @@
+﻿namespace HelloWorldLibrary
+{
+    public class ConcatLogic
+    {
+        public ConcatLogic()
+        {
+            CurrentTime = DateTime.Now;
+        }
+
+        public DateTime CurrentTime { get; set; }
+
+        public string Concat(string username)
+        {
+            var result = $"{CurrentTime} Hello, {username}!";
+            
+            return result;
+        }
+
+        public string ConcatTypeTwo(string username)
+        {
+            var result = CurrentTime + " Hello, " + username + "!";
+           
+            return result;
+        }
+    }
+}
