11bfcd1db037e6f14989ef97387e5815fea07391
Ceren Susuz <ceren_susuz@epam.com>
2023-10-24 11:05:46 +0300

    created presentation layer and basic class

---
+++ AdvancedC#/FileSystemVisitorPL/FileSystemVisitor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace FileSystemVisitorPL
+{
+    public class FileSystemVisitor
+    {
+        private readonly string rootFolder;
+
+        public FileSystemVisitor(string rootPath)
+        {
+            if (!Directory.Exists(rootPath))
+            {
+                throw new DirectoryNotFoundException("The specified root folder does not exist.");
+            }
+
+            rootFolder = rootPath;
+        }
+
+        public IEnumerable<string> GetFilesAndFolders()
+        {
+            return TraverseDirectory(rootFolder);
+        }
+
+        private IEnumerable<string> TraverseDirectory(string directory)
+        {
+            yield return directory;
+
+            foreach (string subdirectory in Directory.GetDirectories(directory))
+            {
+                foreach (string item in TraverseDirectory(subdirectory))
+                {
+                    yield return item;
+                }
+            }
+
+            foreach (string file in Directory.GetFiles(directory))
+            {
+                yield return file;
+            }
+        }
+    }
+}
---
+++ AdvancedC#/FileSystemVisitorPL/Program.cs
@@ -0,0 +1,8 @@
+﻿using FileSystemVisitorPL;
+
+FileSystemVisitor fileSystemVisitor = new("C:\\Users\\Ceren_Susuz\\Documents\\Work_Hard\\NET");
+
+foreach (string item in fileSystemVisitor.GetFilesAndFolders())
+{
+    Console.WriteLine(item);
+}
