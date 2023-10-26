4e5356b9a5ed90a262f2b85c0b978e77e886bd76
Ceren Susuz <ceren_susuz@epam.com>
2023-10-26 18:41:16 +0300

    added missing folders

---
+++ AdvancedC#/FileSystemVisitorPL/Program.cs
@@ -0,0 +1,34 @@
+﻿using FileSystemVisitorPL;
+
+Console.Write("Enter the root folder path: ");
+string rootPath = Console.ReadLine();
+
+Console.Write("Enter a filter (e.g., '.txt' or 'cs'): ");
+string searchFilter = Console.ReadLine();
+
+Func<string, bool> customFilter = item =>
+{
+    if (string.IsNullOrWhiteSpace(searchFilter))
+    {
+        return true;
+    }
+    string extension = Path.GetExtension(item);
+    
+    return !string.IsNullOrWhiteSpace(extension) && extension.Equals(searchFilter, StringComparison.OrdinalIgnoreCase);
+};
+
+if (string.IsNullOrEmpty(rootPath))
+{
+    Console.WriteLine("Invalid root folder path.");
+    
+    return;
+}
+
+var fileSystemVisitor = new FileSystemVisitor(rootPath, customFilter);
+
+foreach (string item in fileSystemVisitor.GetFilesAndFolders())
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadKey();
