23d9f5835f43a2feb6ac4f9986e99fb98c8e6573
Ceren Susuz <ceren_susuz@epam.com>
2023-10-26 18:12:53 +0300

    refactoring folders

--- FundamentalsTasks/ConsoleApp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using StandardLibraryConcat;
-
-string username = Console.ReadLine();
-Console.WriteLine($"Hello, {username}");
-Console.WriteLine("---------------------");
-ConcatLogic concatenationLogic = new();
-Console.WriteLine(concatenationLogic.Concat(username));
-Console.WriteLine("---------------------");
-Console.WriteLine(concatenationLogic.ConcatTypeTwo(username));
