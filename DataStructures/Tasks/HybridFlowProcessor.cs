cf96f5af58fbdc3d11cdbb508a220113b42c7840
Ceren Susuz <ceren_susuz@epam.com>
2023-11-28 00:45:57 +0300

    did part2

--- DataStructures/Tasks/HybridFlowProcessor.cs
+++ DataStructures/Tasks/HybridFlowProcessor.cs
@@ -5,24 +5,47 @@ namespace Tasks
 {
     public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
     {
+        private readonly DoublyLinkedList<T> storage;
+
+        public HybridFlowProcessor()
+        {
+            storage = new DoublyLinkedList<T>();
+        }
+
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (storage.Length == 0)
+            {
+                throw new InvalidOperationException("HybridFlowProcessor is empty.");
+            }
+
+            T item = storage.ElementAt(0);
+            storage.RemoveAt(0);
+
+            return item;
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            storage.Add(item);
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if (storage.Length == 0)
+            {
+                throw new InvalidOperationException("HybridFlowProcessor is empty.");
+            }
+
+            T item = storage.ElementAt(storage.Length - 1);
+            storage.RemoveAt(storage.Length - 1);
+
+            return item;
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            storage.Add(item);
         }
     }
 }
