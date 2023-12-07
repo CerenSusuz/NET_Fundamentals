1b786ba45bd31a052379d53daefb9d1c29577596
Ceren Susuz <ceren_susuz@epam.com>
2023-12-07 11:32:33 +0300

    Add FizzBuzz computation logic to FizzBuzzTask

---
+++ FizzBuzzConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FizzBuzzTask;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fizzBuzz = new FizzBuzzTask();
+            PrintFizzBuzzResults(fizzBuzz);
+        }
+
+        private static void PrintFizzBuzzResults(FizzBuzzTask fizzBuzz)
+        {
+            for (int number = 1; number <= 100; number++)
+            {
+                string result = fizzBuzz.ComputeFizzBuzz(number);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
