a54906780ca7ee6f1c8bdb6fb725b9370a2ee8f3
Ceren Susuz <ceren_susuz@epam.com>
2023-12-07 12:10:37 +0300

    Add PrimeCompositeTask class with methods for prime and composite numbers

---
+++ Unit Testing/PrimeFactorKata/PrimeFactorKata/PrimeCompositeTask.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactorKata
+{
+    public class PrimeCompositeTask
+    {
+        public bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+            else if (number == 2)
+            {
+                return true;
+            }
+            else if (number % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 3; i * i <= number; i += 2)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        public bool IsComposite(int number)
+        {
+            if (number < 4)
+            {  
+                return false; 
+            }
+
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public string PrimeCompositeCheck(int start, int end)
+        {
+            StringBuilder result = new();
+
+            for (int number = start; number <= end; number++)
+            {
+                if (IsPrime(number))
+                {
+                    result.AppendLine("Prime");
+                }
+                else if (IsComposite(number) && !IsEven(number))
+                {
+                    result.AppendLine("Composite");
+                }
+                else
+                {
+                    result.AppendLine(number.ToString());
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
