70805c9cb3a6df3d428f0d2c99d4f2960b2c319b
Ceren Susuz <ceren_susuz@epam.com>
2023-12-11 23:28:28 +0300

    Created trigger for Employee table to add Company on insert

---
+++ SqlFundamentalsDemo/SqlDemo/trg_InsertEmployee.sql
@@ -0,0 +1,21 @@
+﻿CREATE TRIGGER [trg_InsertEmployee]
+	ON [dbo].[Employee]
+	FOR DELETE, INSERT, UPDATE
+	AS
+	BEGIN
+		SET NOCOUNT ON
+		
+		DECLARE @InsertedAddressId int;
+		SELECT @InsertedAddressId = AddressId FROM inserted;
+
+		DECLARE @CompanyName nvarchar(20);
+		SELECT @CompanyName = CompanyName FROM inserted;
+
+		DECLARE @AddressId int;
+		INSERT INTO dbo.Address (Street, City, State, ZipCode)
+			SELECT Street, City, State, ZipCode FROM dbo.Address WHERE Id = @InsertedAddressId;
+		SET @AddressId = SCOPE_IDENTITY();
+    
+		INSERT INTO dbo.Company (Name, AddressId) 
+		VALUES (@CompanyName, @AddressId);
+	END
