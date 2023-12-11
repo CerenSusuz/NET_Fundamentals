a40516a08841ca0e4c13ea2947aed32d3dddabc8
Ceren Susuz <ceren_susuz@epam.com>
2023-12-11 20:57:12 +0300

    Created postdeploy.sql

---
+++ SqlFundamentalsDemo/SqlDemo/Script.postdeploy.sql
@@ -0,0 +1,19 @@
+﻿INSERT INTO [dbo].[Person] (Id, FirstName, LastName) 
+VALUES 
+(1, 'John', 'Doe'),
+(2, 'Jane', 'Smith')
+
+INSERT INTO [dbo].[Address] (Id, Street, City, State, ZipCode) 
+VALUES 
+(1, 'Baker Street', 'London', 'England', 'NW1 6XE'),
+(2, 'Broadway', 'New York', 'NY', '10012')
+
+INSERT INTO [dbo].[Employee] (Id, AddressId, PersonId, CompanyName, Position, EmployeeName)
+VALUES 
+(1, 1, 1, 'Company A', 'Manager', 'John Doe'),
+(2, 2, 2, 'Company B', 'Assistant', 'Jane Smith')
+
+INSERT INTO [dbo].[Company] (Id, Name, AddressId)
+VALUES 
+(1, 'Company A', 1),
+(2, 'Company B', 2)
