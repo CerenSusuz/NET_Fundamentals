fa5844cfae4061a2839da69f13a2a8b4dd002ffe
Ceren Susuz <ceren_susuz@epam.com>
2023-12-11 23:44:21 +0300

    Updated unit tests to include Arrange-Act-Assert pattern

--- Unit Testing/KatasTests/OddEvenTests.cs
+++ Unit Testing/KatasTests/OddEvenTests.cs
@@ -1,9 +1,4 @@
 ﻿using OddEvenKata;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KatasTests
 {
@@ -19,28 +14,39 @@ namespace KatasTests
         [Fact]
         public void OddEvenPrimeCheck_1To10_ReturnsExpectedResults()
         {
+            //Arrange
             string expectedResult = "Odd\r\nEven\r\n3\r\nEven\r\n5\r\nEven\r\n7\r\nEven\r\nOdd\r\nEven\r\n";
+
+            //Act 
             string result = _oddEvenTask.OddEvenPrimeCheck(1, 10);
 
+            //Assert
             Assert.Equal(expectedResult, result);
         }
 
         [Fact]
         public void OddEvenPrimeCheck_11To20_ReturnsExpectedResults()
         {
-            string expectedResult =
-                "11\r\nEven\r\n13\r\nEven\r\nOdd\r\nEven\r\n17\r\nEven\r\n19\r\nEven\r\n";
+            //Arrange
+            string expectedResult = "11\r\nEven\r\n13\r\nEven\r\nOdd\r\nEven\r\n17\r\nEven\r\n19\r\nEven\r\n";
+
+            //Act
             string result = _oddEvenTask.OddEvenPrimeCheck(11, 20);
 
+            //Assert
             Assert.Equal(expectedResult, result);
         }
 
         [Fact]
         public void OddEvenPrimeCheck_21To30_ReturnsExpectedResults()
         {
+            //Arrange
             string expectedResult = "Odd\r\nEven\r\n23\r\nEven\r\nOdd\r\nEven\r\nOdd\r\nEven\r\n29\r\nEven\r\n";
+
+            //Act
             string result = _oddEvenTask.OddEvenPrimeCheck(21, 30);
 
+            //Assert
             Assert.Equal(expectedResult, result);
         }
 
@@ -51,9 +57,11 @@ namespace KatasTests
         [InlineData(9, "Odd\r\n")]
         public void OddEvenPrimeCheck_SingleNumber_ReturnsExpectedResult(int number, string expectedResult)
         {
+            //Act
             string result = _oddEvenTask.OddEvenPrimeCheck(number, number);
+
+            //Assert
             Assert.Equal(expectedResult, result);
         }
-
     }
 }
