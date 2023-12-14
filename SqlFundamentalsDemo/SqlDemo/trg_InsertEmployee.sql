6c9b4240dbd58cf79c054427999c455873e4e959
Ceren Susuz <ceren_susuz@epam.com>
2023-12-14 13:45:45 +0300

    Modified database trigger

--- SqlFundamentalsDemo/SqlDemo/trg_InsertEmployee.sql
+++ SqlFundamentalsDemo/SqlDemo/trg_InsertEmployee.sql
@@ -1,21 +1,21 @@
 ﻿CREATE TRIGGER [trg_InsertEmployee]
-	ON [dbo].[Employee]
-	FOR DELETE, INSERT, UPDATE
-	AS
-	BEGIN
-		SET NOCOUNT ON
-		
-		DECLARE @InsertedAddressId int;
-		SELECT @InsertedAddressId = AddressId FROM inserted;
+    ON [dbo].[Employee]
+    FOR DELETE, INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON
 
-		DECLARE @CompanyName nvarchar(20);
-		SELECT @CompanyName = CompanyName FROM inserted;
+    DECLARE @InsertedAddressId int;
+    SELECT @InsertedAddressId = AddressId FROM inserted;
 
-		DECLARE @AddressId int;
-		INSERT INTO dbo.Address (Street, City, State, ZipCode)
-			SELECT Street, City, State, ZipCode FROM dbo.Address WHERE Id = @InsertedAddressId;
-		SET @AddressId = SCOPE_IDENTITY();
-    
-		INSERT INTO dbo.Company (Name, AddressId) 
-		VALUES (@CompanyName, @AddressId);
-	END
+    DECLARE @CompanyName nvarchar(20);
+    SELECT @CompanyName = CompanyName FROM inserted;
+
+    DECLARE @AddressId int;
+    INSERT INTO dbo.Address (Street, City, State, ZipCode)
+        SELECT Street, City, State, ZipCode FROM dbo.Address WHERE Id = @InsertedAddressId;
+    SET @AddressId = SCOPE_IDENTITY();
+
+    INSERT INTO dbo.Company (Name, AddressId) 
+    VALUES (@CompanyName, @AddressId);
+END
