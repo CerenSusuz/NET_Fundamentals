eccdf6ede95be8f64bf1b81269329b4327a7d5ee
Ceren Susuz <ceren_susuz@epam.com>
2023-12-14 13:38:39 +0300

    Updated the SQL script with proper indentation, for improved readabilit

--- SqlFundamentalsDemo/SqlDemo/EmployeeInfo.sql
+++ SqlFundamentalsDemo/SqlDemo/EmployeeInfo.sql
@@ -1,10 +1,11 @@
 ﻿CREATE VIEW [dbo].[EmployeeInfo]
-	AS SELECT 
+AS 
+SELECT 
     E.Id AS EmployeeId,
-	ISNULL(E.EmployeeName, (P.FirstName + ' ' + P.LastName)) AS EmployeeFullName, 
-	(A.ZipCode + '_' + A.State + ', ' + A.City + '-' + A.Street) AS EmployeeFullAddress,
-	(E.CompanyName + '(' + E.Position + ')') AS EmployeeCompanyInfo
-	
-	FROM [dbo].[Employee] E
-	INNER JOIN [dbo].[Person] P ON E.PersonId = P.Id
-	INNER JOIN [dbo].[Address] A ON E.AddressId = A.Id
+    ISNULL(E.EmployeeName, (P.FirstName + ' ' + P.LastName)) AS EmployeeFullName, 
+    (A.ZipCode + '_' + A.State + ', ' + A.City + '-' + A.Street) AS EmployeeFullAddress,
+    (E.CompanyName + '(' + E.Position + ')') AS EmployeeCompanyInfo
+FROM 
+    [dbo].[Employee] E
+    INNER JOIN [dbo].[Person] P ON E.PersonId = P.Id
+    INNER JOIN [dbo].[Address] A ON E.AddressId = A.Id
