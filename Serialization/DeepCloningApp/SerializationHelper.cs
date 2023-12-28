de989f61e688f93a0f69e815f0e92d1e0d79332a
Ceren Susuz <ceren_susuz@epam.com>
2023-12-28 14:08:49 +0300

    task 3: Implemented deep cloning for Employee and Department using JSON serialization

---
+++ Serialization/DeepCloningApp/SerializationHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace DeepCloningApp
+{
+    public static class SerializationHelper
+    {
+        public static T DeepCopyWithJson<T>(T obj)
+        {
+            var jsonString = JsonSerializer.Serialize(value: obj);
+            
+            return JsonSerializer.Deserialize<T>(json: jsonString);
+        }
+    }
+}
