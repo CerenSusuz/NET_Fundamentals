1382f0248750bfd6a6081a30d00115bcc5a83eaa
Ceren Susuz <ceren_susuz@epam.com>
2023-12-28 14:07:55 +0300

    task 1: Implemented serialization/deserialization for Employee and Department with Binary, XML and JSON formats.

---
+++ Serialization/XMLSerialization/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Serialization;
+using XMLSerialization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Department department = new()
+        {
+            DepartmentName = "Development",
+            Employees = [new Employee { EmployeeName = "Ceren" }]
+        };
+
+        var serializer = new XmlSerializer(typeof(Department));
+
+        using (TextWriter writer = new StreamWriter(@"./department.xml"))
+        {
+            serializer.Serialize(writer, department);
+        }
+
+        Department? deserialized;
+
+        using (TextReader reader = new StreamReader(@"./department.xml"))
+        {
+            deserialized = serializer.Deserialize(textReader: reader) as Department;
+        }
+
+        Console.WriteLine($"Deserialized department: {deserialized.DepartmentName}");
+    }
+}
