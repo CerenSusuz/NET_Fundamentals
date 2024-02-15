087688261453f70dbc4fdda00c9b94d70bcea492
Ceren Susuz <ceren_susuz@epam.com>
2024-02-16 00:09:32 +0300

    Implemented HTTP client-server communication

---
+++ HTTP/HttpClientApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static readonly HttpClient client = new();
+
+    static async Task Main(string[] args)
+    {
+        HttpResponseMessage response = await client.GetAsync("http://localhost:8888/MyName");
+
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine("Received response: " + responseBody);
+
+        Console.ReadLine();
+    }
+}
---
+++ HTTP/HttpListenerApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        HttpListener server = new();
+        server.Prefixes.Add("http://localhost:8888/");
+        server.Start();
+
+        Console.WriteLine("Server started...");
+
+        while (true)
+        {
+            HttpListenerContext context = await server.GetContextAsync();
+            HttpListenerRequest request = context.Request;
+
+            string methodName = request.RawUrl.Trim('/');
+            switch (methodName)
+            {
+                case "MyName":
+                    await GetMyName(context.Response);
+                    break;
+
+                default:
+                    context.Response.StatusCode = 404;
+                    break;
+            }
+        }
+    }
+
+    static async Task GetMyName(HttpListenerResponse response)
+    {
+        string name = "Gerund";
+        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(name);
+        response.ContentLength64 = buffer.Length;
+        await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+        response.Close();
+    }
+}
