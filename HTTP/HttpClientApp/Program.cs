9f9dee65d01e630ed1a63601ccf2578a5167160d
Ceren Susuz <ceren_susuz@epam.com>
2024-02-16 00:33:14 +0300

    implement requests to 5 URLs implement 5 methods that will form a response for the urls above with different statuses

--- HTTP/HttpClientApp/Program.cs
+++ HTTP/HttpClientApp/Program.cs
@@ -5,16 +5,44 @@ using System.Threading.Tasks;
 class Program
 {
     static readonly HttpClient client = new();
+    private static readonly string[] urls =
+    {
+        "http://localhost:8888/MyName",
+        "http://localhost:8888/Information",
+        "http://localhost:8888/Success",
+        "http://localhost:8888/Redirection",
+        "http://localhost:8888/ClientError",
+        "http://localhost:8888/ServerError"
+    };
 
     static async Task Main(string[] args)
     {
-        HttpResponseMessage response = await client.GetAsync("http://localhost:8888/MyName");
-
-        response.EnsureSuccessStatusCode();
-        string responseBody = await response.Content.ReadAsStringAsync();
-
-        Console.WriteLine("Received response: " + responseBody);
+        await FetchAndPrintResponsesFromUrls();
 
         Console.ReadLine();
+    }
+
+    private static async Task FetchAndPrintResponsesFromUrls()
+    {
+        foreach (string url in urls)
+        {
+            await FetchAndPrintResponseFromUrl(url);
+        }
+    }
+
+    private static async Task FetchAndPrintResponseFromUrl(string url)
+    {
+        HttpResponseMessage response = await client.GetAsync(url);
+        Console.WriteLine($"For url: {url}, received status code: {response.StatusCode}");
+        await MaybePrintResponseContent(url, response);
+    }
+
+    private static async Task MaybePrintResponseContent(string url, HttpResponseMessage response)
+    {
+        if (url.EndsWith("MyName", StringComparison.OrdinalIgnoreCase) && response.IsSuccessStatusCode)
+        {
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Received name: " + responseBody);
+        }
     }
 }
--- HTTP/HttpListenerApp/Program.cs
+++ HTTP/HttpListenerApp/Program.cs
@@ -4,39 +4,74 @@ using System.Threading.Tasks;
 
 class Program
 {
+    private const string URL = "http://localhost:8888/";
+    private static HttpListener? server;
+
     static async Task Main(string[] args)
     {
-        HttpListener server = new();
-        server.Prefixes.Add("http://localhost:8888/");
-        server.Start();
-
-        Console.WriteLine("Server started...");
+        StartServer();
 
         while (true)
         {
-            HttpListenerContext context = await server.GetContextAsync();
-            HttpListenerRequest request = context.Request;
-
-            string methodName = request.RawUrl.Trim('/');
-            switch (methodName)
-            {
-                case "MyName":
-                    await GetMyName(context.Response);
-                    break;
-
-                default:
-                    context.Response.StatusCode = 404;
-                    break;
-            }
+            await HandleIncomingRequest();
         }
     }
 
-    static async Task GetMyName(HttpListenerResponse response)
+    private static void StartServer()
+    {
+        server = new HttpListener();
+        server.Prefixes.Add(URL);
+        server.Start();
+
+        Console.WriteLine("Server started...");
+    }
+
+    private static async Task HandleIncomingRequest()
+    {
+        HttpListenerContext context = await server.GetContextAsync();
+        HttpListenerRequest request = context.Request;
+
+        string resourceName = request.RawUrl.Trim('/');
+
+        switch (resourceName)
+        {
+            case "MyName":
+                await SendNameInResponse(context.Response);
+                break;
+            case "Information":
+                SetResponseStatus(context.Response, 100);
+                break;
+            case "Success":
+                SetResponseStatus(context.Response, 200);
+                break;
+            case "Redirection":
+                SetResponseStatus(context.Response, 300);
+                break;
+            case "ClientError":
+                SetResponseStatus(context.Response, 400);
+                break;
+            case "ServerError":
+                SetResponseStatus(context.Response, 500);
+                break;
+            default:
+                SetResponseStatus(context.Response, 404);
+                break;
+        }
+
+        context.Response.Close();
+    }
+
+    private static void SetResponseStatus(HttpListenerResponse response, int statusCode)
+    {
+        response.StatusCode = statusCode;
+    }
+
+    private static async Task SendNameInResponse(HttpListenerResponse response)
     {
         string name = "Gerund";
         byte[] buffer = System.Text.Encoding.UTF8.GetBytes(name);
         response.ContentLength64 = buffer.Length;
+
         await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
-        response.Close();
     }
 }
